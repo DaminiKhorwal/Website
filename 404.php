bf870e9885857d16781d16dcf62c8328d5228e2e
DaminiKhorwal <daminikhorwal118@gmail.com>
2024-12-11 19:58:27 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?= Template::Header(['title' => 'We Couldn’t Find That Document', 'highlight' => '', 'description' => 'We couldn’t find that document.', 'isErrorPage' => true]) ?>
+<main>
+	<section class="narrow has-hero">
+		<hgroup>
+			<h1>We Couldn’t Find That Document</h1>
+			<h2>This is a 404 error.</h2>
+		</hgroup>
+		<picture data-caption="Blind Orion Searching for the Rising Sun. Nicolas Poussin, 1658">
+			<source srcset="/images/blind-orion@2x.avif 2x, /images/blind-orion.avif 1x" type="image/avif"/>
+			<source srcset="/images/blind-orion@2x.jpg 2x, /images/blind-orion.jpg 1x" type="image/jpg"/>
+			<img src="/images/blind-orion@2x.jpg" alt="A classical city is aflame as people scramble in the foreground."/>
+		</picture>
+		<p>We couldn’t find a document at the URL you specified. Did you mistype the URL?</p>
+		<p>If you arrived here from a link on the Standard Ebooks website, please <a href="/about#editor-in-chief">contact us</a> so that we can fix it.</p>
+		<p>If you arrived here from a link elsewhere on the web, please contact the site you came from to ask them to fix their link.</p>
+	</section>
+</main>
+<?= Template::Footer() ?>
---
+++ 451.php
@@ -0,0 +1,16 @@
+<?= Template::Header(['title' => 'This Ebook Is No Longer Available', 'highlight' => '', 'description' => 'This ebook is unavailable due to legal reasons.']) ?>
+<main>
+	<section class="narrow has-hero">
+		<hgroup>
+			<h1>This Ebook Is No Longer Available</h1>
+			<h2>This is a 451 error.</h2>
+		</hgroup>
+		<picture data-caption="The Preaching of St. Paul at Ephesus. Eustache Le Sueur, 1649">
+			<source srcset="/images/the-preaching-of-st-paul-at-ephesus@2x.avif 2x, /images/the-preaching-of-st-paul-at-ephesus.avif 1x" type="image/avif"/>
+			<source srcset="/images/the-preaching-of-st-paul-at-ephesus@2x.jpg 2x, /images/the-preaching-of-st-paul-at-ephesus.jpg 1x" type="image/jpg"/>
+			<img src="/images/the-preaching-of-st-paul-at-ephesus@2x.jpg" alt="A classical city is aflame as people scramble in the foreground."/>
+		</picture>
+		<p>This ebook is no longer available, due to legal reasons. This may occur if the book was thought to be in the U.S. public domain, but new evidence changed that determination.</p>
+	</section>
+</main>
+<?= Template::Footer() ?>
